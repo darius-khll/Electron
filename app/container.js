eb96d9cbf4c41d05c15a458f42a62dc51b1687a1
Ali Khalili <ali.khll.1994@gmail.com>
2017-04-06 20:40:30 +0430

    container added

---
+++ app/container.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', ["ngComponentRouter"]);
+app.config(function ($locationProvider) {
+    //$locationProvider.html5Mode(true);
+});
+app.value('$routerRootComponent', 'app');
+app.component('app', {
+    template: `<nav>
+      <a ng-link="['First']">First</a>
+      <a ng-link="['Second']">Second</a>
+      
+    </nav>
+    <ng-outlet></ng-outlet>`,
+    $routeConfig: [
+        { path: '/first', name: 'First', component: 'first', useAsDefault: true },
+        { path: '/second', name: 'Second', component: 'second' },
+        { path: '**', component: 'first' }
+    ]
+});
+//# sourceMappingURL=container.js.map
